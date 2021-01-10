415ee80d6d53aa5740373ae93d57a09333e6eed3
Andrii Dorohov <wedpositive@gmail.com>
2021-01-10 22:27:37 +0200

    t
    t

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,16 @@
+//Напиши функцию - конструктор Account, которая создает объект со свойствами login и email.
+//В prototype функции - конструктора добавь метод getInfo(),
+//который выводит в консоль значения полей login и email объекта который его вызвал.
+
+const Account = function (login, email) {
+  this.login = login;
+  this.email = email;
+};
+
+Account.prototype.getInfo = function () {
+  console.log(this.login, this.email);
+};
+
 console.log(Account.prototype.getInfo); // function
 
 const mango = new Account({
