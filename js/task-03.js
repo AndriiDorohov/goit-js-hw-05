c5b1b7a6dd49332d85eacc0c9ba4633001cb5887
Andrii Dorohov <wedpositive@gmail.com>
2021-01-11 02:57:06 +0200

    t

--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,28 @@
+//Напиши класс Storage, который будет создавать объекты для управления складом товаров.
+//При вызове будет получать один аргумент - начальный массив товаров, и записывать его в свойство items.
+//Добавь методы класса:
+
+//getItems() - возвращает массив текущих товаров
+//addItem(item) - получает новый товар и добавляет его к текущим
+//removeItem(item) - получет товар и, если он есть, удаляет его из текущих
+
+class Storage {
+  constructor(goods) {
+    this.items = goods;
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem(item) {
+    this.items.push(item);
+  }
+  removeItem(item) {
+    while (this.items.includes(item)) {
+      this.items.splice(this.items.indexOf(item), 1);
+    }
+  }
+}
+
 const storage = new Storage([
   'Нанитоиды',
   'Пролонгер',
